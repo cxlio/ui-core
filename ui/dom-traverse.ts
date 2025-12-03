4576f021370625854d8ecb22db005fc087deb71e
Giancarlo Bellido <giancarlo@coaxialhost.com>
2025-12-03 16:48:53 -0500

    feat: dom utils

---
+++ ui/dom-traverse.ts
@@ -0,0 +1,23 @@
+export function walkUp(
+	start: Element,
+	pred: (node: Element) => boolean,
+): Element | undefined {
+	let current = start;
+
+	while (current) {
+		if (pred(current)) return current;
+
+		if (current.parentElement) {
+			current = current.parentElement;
+			continue;
+		}
+
+		const root = current.getRootNode();
+		if (root instanceof ShadowRoot) {
+			current = root.host;
+			continue;
+		}
+
+		break;
+	}
+}
